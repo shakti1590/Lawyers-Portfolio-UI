52522ca624d9fe1023c0c9d001d4df071d0704fc
shakti singh <ssingh12102002@gmail.com>
2024-06-27 08:03:25 +0530

    add call to action

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,42 @@ const Footer = () => {
   return (
         <footer className="w-full py-14">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="border-b border-gray-200 pb-14 flex justify-between items-center flex-col gap-8 lg:gap-0 lg:flex-row">
-                <div className="block">
-                    <h3 className="font-manrope text-3xl text-gray-900 font-bold mb-2 text-center lg:text-left">Subscribe to our newsletter</h3>
-                    <p className="text-gray-500 text-center lg:text-left">Stay up to date with the latest update and announcement.</p>
-                </div>
-                <div className="flex items-center flex-col gap-4 lg:flex-row">
-                    <input type="text" name="email" className="py-3 px-6 h-14 border border-gray-300 shadow-sm rounded-full focus:outline-none" placeholder="Enter your mail.."/>
-                    <button className="h-14 py-3.5 px-7 bg-indigo-600 shadow-sm rounded-full text-white font-bold">Subscribe</button>
-                </div>
-            </div>
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div
+          className="lg:py-14 lg:px-20 p-10 rounded-2xl bg-gradient-to-r from-indigo-600 to-violet-600 flex items-center justify-between flex-col lg:flex-row"
+        >
+          <div className="block text-center mb-5 lg:text-left lg:mb-0">
+            <h2
+              className="font-manrope text-4xl text-white font-semibold mb-5 lg:mb-2"
+            >
+              Connect with us
+            </h2>
+            <p className="text-xl text-indigo-100">
+              Contact us with any query or any idea.
+            </p>
+          </div>
+          <a
+            href="#"
+            className="flex items-center gap-2 bg-white rounded-full shadow-sm text-lg text-indigo-600 font-semibold py-4 px-8 transition-all duration-500"
+            >Get In Touch
+            <svg
+              width="19"
+              height="14"
+              viewBox="0 0 19 14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.75 7L16.4167 7M11.8333 12.5L16.6852 7.64818C16.9907 7.34263 17.1435 7.18985 17.1435 7C17.1435 6.81015 16.9907 6.65737 16.6852 6.35182L11.8333 1.5"
+                stroke="#4F46E5"
+                stroke-width="2.4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
             <div className="grid grid-cols-2 min-[690px]:grid-cols-4 lg:grid-cols-6 gap-4 xl:gap-8 pt-14 pb-10 max-w-xs mx-auto min-[690px]:max-w-2xl lg:max-w-full">
                 <div className="col-span-full mb-10 lg:col-span-2 lg:mb-0">
                     <a href="#"  className="flex justify-center lg:justify-start">
