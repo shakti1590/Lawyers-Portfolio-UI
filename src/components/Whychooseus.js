bcca32eb908f811c9652813cad4a254da6a997c1
shakti singh <ssingh12102002@gmail.com>
2024-06-27 08:09:25 +0530

    remove button

--- src/components/Whychooseus.js
+++ src/components/Whychooseus.js
@@ -19,9 +19,6 @@ const Whychoose = () => {
           <p className="text-gray-500 text-sm">
             Laboris qui Lorem ad tempor ut reprehenderit. Nostrud anim nulla officia ea sit deserunt. Eu eu quis anim aute Laboris qui Lorem ad tempor ut reprehenderit.
           </p>
-          <button type="button" className="border px-4 py-2 rounded-md font-bold flex items-center text-sm mt-6 hover:bg-gray-50">
-            Learn more <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" className="rotate-90 ml-1.5"><path fill="#333" d="M12.006 1a1 1 0 0 1 .838.463l7 11a1 1 0 0 1-.985 1.527l-3.364-.48a.434.434 0 0 0-.495.43V20c0 1.645-1.355 3-3 3s-3-1.355-3-3v-6.06a.434.434 0 0 0-.495-.43l-3.364.48a1 1 0 0 1-.985-1.527l7-11a1 1 0 0 1 .85-.463z" /></svg>
-          </button>
         </div>
         <div className="text-left bg-blue-50 rounded-md p-6">
           <svg xmlns="http://www.w3.org/2000/svg" className="w-12 inline-block bg-white p-3 rounded-full" viewBox="0 0 682.667 682.667">
@@ -39,9 +36,6 @@ const Whychoose = () => {
           <p className="text-gray-500 text-sm">
             Laboris qui Lorem ad tempor ut reprehenderit. Nostrud anim nulla officia ea sit deserunt. Eu eu quis anim aute Laboris qui Lorem ad tempor ut reprehenderit.
           </p>
-          <button type="button" className="border px-4 py-2 rounded-md font-bold flex items-center text-sm mt-6 hover:bg-gray-50">
-            Learn more <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" className="rotate-90 ml-1.5"><path fill="#333" d="M12.006 1a1 1 0 0 1 .838.463l7 11a1 1 0 0 1-.985 1.527l-3.364-.48a.434.434 0 0 0-.495.43V20c0 1.645-1.355 3-3 3s-3-1.355-3-3v-6.06a.434.434 0 0 0-.495-.43l-3.364.48a1 1 0 0 1-.985-1.527l7-11a1 1 0 0 1 .85-.463z" /></svg>
-          </button>
         </div>
         <div className="text-left bg-blue-50 rounded-md p-6">
           <svg xmlns="http://www.w3.org/2000/svg" className="fill-blue-600 w-12 inline-block bg-white p-3 rounded-full" viewBox="0 0 64 64"><path d="M62 2H40c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h4v10.2c-1.3-.8-2.8-1.2-4.4-1.2-4.8 0-8.6 3.8-8.6 8.6S35 48.8 39.8 48.8s8.6-3.8 8.6-8.6V22h4c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 16H42V6h18v12zM39.8 46.8c-3.4 0-6.2-2.8-6.2-6.2s2.8-6.2 6.2-6.2 6.2 2.8 6.2 6.2-2.8 6.2-6.2 6.2zm-23.6 1.2c-1.6 0-3.1.4-4.4 1.2V14h4c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H2c-1.1 0-2 .9-2 2v6c0 1.1.9 2 2 2h4v36.2c-1.3-.8-2.8-1.2-4.4-1.2C.8 49 0 49.8 0 50.8v10.4c0 1 .8 1.8 1.8 1.8 1.6 0 3.1-.4 4.4-1.2V54h12v7.8c1.3.8 2.8 1.2 4.4 1.2 1 0 1.8-.8 1.8-1.8V50.8c0-1-.8-1.8-1.8-1.8zM2 12V6h18v6H2zm4.4 38H4v-4.4c3.4 0 6.2 2.8 6.2 6.2s-2.8 6.2-6.2 6.2v-4.4h2.4V44zm6.6-20V22h4c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h4v18.2c-1.3-.8-2.8-1.2-4.4-1.2C.8 39 0 39.8 0 40.8v10.4c0 1 .8 1.8 1.8 1.8 1.6 0 3.1-.4 4.4-1.2V44h12v7.8c1.3.8 2.8 1.2 4.4 1.2 1 0 1.8-.8 1.8-1.8V40.8c0-1-.8-1.8-1.8-1.8zm4-2h18v-2H14v-4h6v-2h-6v-2h18v-2H14v-2h6v-2h-6v-2h18v-2H14v-2h6v-2h-6V6h18v6h-18z"/></svg>
@@ -49,9 +43,6 @@ const Whychoose = () => {
           <p className="text-gray-500 text-sm">
             Laboris qui Lorem ad tempor ut reprehenderit. Nostrud anim nulla officia ea sit deserunt. Eu eu quis anim aute Laboris qui Lorem ad tempor ut reprehenderit.
           </p>
-          <button type="button" className="border px-4 py-2 rounded-md font-bold flex items-center text-sm mt-6 hover:bg-gray-50">
-            Learn more <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" className="rotate-90 ml-1.5"><path fill="#333" d="M12.006 1a1 1 0 0 1 .838.463l7 11a1 1 0 0 1-.985 1.527l-3.364-.48a.434.434 0 0 0-.495.43V20c0 1.645-1.355 3-3 3s-3-1.355-3-3v-6.06a.434.434 0 0 0-.495-.43l-3.364.48a1 1 0 0 1-.985-1.527l7-11a1 1 0 0 1 .85-.463z"/></svg>
-          </button>
         </div>
         <div className="text-left bg-blue-50 rounded-md p-6">
           <svg xmlns="http://www.w3.org/2000/svg" className="w-12 inline-block bg-white p-3 rounded-full" viewBox="0 0 64 64"><path fill="#000" d="M52.73 6.302a1 1 0 0 0-1.732 1L55.268 16H48a1 1 0 1 0 0 2h9a1 1 0 0 0 .894-1.447l-6-10z"/><path fill="#000" d="M33 5.002h-2a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2zM44 8.002h-4a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2zM55 11.002h-4a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2zM33 9.002h-8a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2zM31 13.002h-6a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2z"/><path fill="#000" d="M60.715 43.283c-1.178-1.178-3.085-1.178-4.263 0L44.344 55.391c-1.178 1.178-1.178 3.085 0 4.263a3.004 3.004 0 0 0 4.263 0l12.108-12.108c1.178-1.178 1.178-3.085 0-4.263zM47.022 57.977a1.004 1.004 0 0 1-1.414 0 1.004 1.004 0 0 1 0-1.414l8.293-8.293a1.004 1.004 0 0 1 1.414 1.414L47.022 57.977zM32 46.002a6.008 6.008 0 0 1 6-6c1.399 0 2.765.488 3.865 1.377l1.836-1.836A8.004 8.004 0 0 0 38 40.002c-4.411 0-8 3.589-8 8a8.004 8.004 0 0 0 7.541 7.962c.245.03.49.04.734.04.014 0 .028-.002.042-.002.551 0 1.09-.058 1.618-.17a7.964 7.964 0 0 0 3.16-1.415l-1.844-1.844A5.994 5.994 0 0 1 38 52.002c-3.309 0-6-2.691-6-6z"/><path fill="#000" d="M59.138 33.79c-.872-.664-1.051-1.929-.387-2.8l2.764-3.63c.012-.017.026-.03.038-.047.15-.199.299-.398.442-.598.7-.988.354-2.357-.734-3.058L41.08 12.65l4.179-5.486c.664-.872.513-2.135-.36-2.799-2.98-2.272-7.122-2.163-9.958.252a12.573 12.573 0 0 0-3.209 4.048c-1.108 2.226-1.546 4.568-1.367 6.944a6.498 6.498 0 0 1-1.128 4.225c-.35.485-.789.918-1.276 1.268l-2.635 1.876-11.133-8.29c-.898-.67-2.17-.478-2.84.421L1.81 38.198c-.67.899-.478 2.171.421 2.84l17.475 12.998c.898.67 2.171.478 2.84-.421l11.129-14.93c.67-.898.478-2.171-.421-2.84l-3.812-2.836 2.633-1.874c.841-.598 1.59-1.316 2.21-2.111a9.32 9.32 0 0 0 1.617-6.067c-.106-1.322.167-2.686.817-3.888 1.518-2.726 5.038-3.583 7.764-2.065l.464.281c.871.664 1.034 1.928.37 2.799l-6.13 8.047 19.803 15.706-1.032 1.355a1 1 0 0 0 1.585 1.216l6.727-8.838c.665-.872.513-2.136-.36-2.799zM17.92 47.813 3.37 36.883l11.136-14.932 11.462 8.528-1.918 1.365-9.23 12.354zm12.4-14.048 3.16 2.348-9.231 12.354-3.159-2.348 9.23-12.354zm11.475 18.996a6.495 6.495 0 0 1-5.63-5.514l3.488-4.671a6.502 6.502 0 0 1 2.524 7.474c-.103.259-.223.514-.367.764l-.015-.053zM34.877 8.074c2.09-1.597 4.991-1.693 7.11-.247L28.207 23.605c.05.01.1.019.151.028l.643.202c1.555.49 3.246.26 4.602-.64 1.04-.686 1.91-1.621 2.531-2.704 1.97-3.534 3.424-6.884 1.088-9.137l-2.759 3.621c-.664.872-1.928 1.034-2.799.37-1.073-.815-1.706-2.063-1.76-3.394-.055-1.337.444-2.664 1.396-3.577z"/><circle fill="#000" cx="42" cy="44" r="2"/></svg>
@@ -59,9 +50,6 @@ const Whychoose = () => {
           <p className="text-gray-500 text-sm">
             Laboris qui Lorem ad tempor ut reprehenderit. Nostrud anim nulla officia ea sit deserunt. Eu eu quis anim aute Laboris qui Lorem ad tempor ut reprehenderit.
           </p>
-          <button type="button" className="border px-4 py-2 rounded-md font-bold flex items-center text-sm mt-6 hover:bg-gray-50">
-            Learn more <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" className="rotate-90 ml-1.5"><path fill="#333" d="M12.006 1a1 1 0 0 1 .838.463l7 11a1 1 0 0 1-.985 1.527l-3.364-.48a.434.434 0 0 0-.495.43V20c0 1.645-1.355 3-3 3s-3-1.355-3-3v-6.06a.434.434 0 0 0-.495-.43l-3.364.48a1 1 0 0 1-.985-1.527l7-11a1 1 0 0 1 .85-.463z"/></svg>
-          </button>
         </div>
       </div>
     </div>
