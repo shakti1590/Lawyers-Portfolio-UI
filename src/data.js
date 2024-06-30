9419b8ccde4306c286fc6526ece1e16cb5a20b1b
shakti singh <ssingh12102002@gmail.com>
2024-07-01 04:42:51 +0530

    remove unused code

--- src/data.js
+++ src/data.js
@@ -3,62 +3,12 @@ import ProfilePhoto1 from '../src/assets/img/profile1.png';
 import ProfilePhoto2 from '../src/assets/img/profile2.png';
 import ProfilePhoto3 from '../src/assets/img/profile3.png';
 
-// import about icons
-import Skill1 from '../src/assets/img/skills/skill1.png';
-import Skill2 from '../src/assets/img/skills/skill2.png';
-import Skill3 from '../src/assets/img/skills/skill3.png';
-import Skill4 from '../src/assets/img/skills/skill4.png';
-import Skill5 from '../src/assets/img/skills/skill5.png';
-import Skill6 from '../src/assets/img/skills/skill6.png';
-import Skill7 from '../src/assets/img/skills/skill6.png';
-import Skill8 from '../src/assets/img/skills/skill6.png';
-import Skill9 from '../src/assets/img/skills/skill6.png';
-
-// import team images
-import TeamMember1 from '../src/assets/img/lawyer1.png';
-import TeamMember2 from '../src/assets/img/lawyer2.png';
-import TeamMember3 from '../src/assets/img/lawyer3.png';
-
 // import social icons
 import FacebookIcon from '../src/assets/img/social/facebook.png';
 import TwitterIcon from '../src/assets/img/social/twitter.png';
 import InstagramIcon from '../src/assets/img/social/instagram.png';
 import YoutubeIcon from '../src/assets/img/social/youtube.png';
 import PinterestIcon from '../src/assets/img/social/pinterest.png';
-
-// import partners 
-import axis from '../src/assets/img/axis-bank.jpg';
-import icic from '../src/assets/img/icici-bank.jpg';
-import hdfc from '../src/assets/img/hdfc.jpeg';
-import kotak from '../src/assets/img/kotak.jpg'
-
-// navigation data
-export const navigation = [
-  {
-    name: 'Home',
-    href: 'home',
-  },
-  {
-    name: 'About',
-    herf: 'about',
-  },
-  {
-    name: 'Services',
-    href: 'skills',
-  },
-  {
-    name: 'Business',
-    href: 'Business',
-  },
-  {
-    name: 'Personal',
-    href: 'Personal',
-  },
-  {
-    name: 'Contact',
-    href: 'contact',
-  },
-];
 
 // testimonials data
 export const testimonials = [
@@ -130,89 +80,6 @@ export const testimonials = [
   },
 ];
 
-// skills data
-export const skills = [
-  {
-    icon: Skill1,
-    title: 'Accounting',
-    description:
-      'Expert accounting services to ensure your financial health and compliance.',
-  },
-  {
-    icon: Skill2,
-    title: 'Labour License',
-    description:
-      'Assistance in obtaining and maintaining labour licenses to meet regulatory requirements.',
-  },
-  {
-    icon: Skill3,
-    title: 'Fassai',
-    description:
-      'Guidance and support for FSSAI registration and compliance for food businesses.',
-  },
-  {
-    icon: Skill4,
-    title: 'Corporate and Business Law',
-    description:
-      'Strategic counsel on corporate governance, transactions, contracts, and compliance.',
-  },
-  {
-    icon: Skill5,
-    title: 'Litigation and Dispute Resolution',
-    description:
-      'Representation and advocacy in civil litigation, arbitration, and alternative dispute resolution.',
-  },
-  {
-    icon: Skill6,
-    title: 'Employment Law',
-    description:
-      'Guidance on employment contracts, workplace policies, and resolution of labor disputes.',
-  },
-  {
-    icon: Skill7,
-    title: 'Intellectual Property',
-    description:
-      'Protection and management of intellectual property rights, including patents, trademarks, and copyrights.',
-  },
-  {
-    icon: Skill8,
-    title: 'Real Estate and Property Law',
-    description:
-      'Assistance with property transactions, leases, zoning issues, and land use regulations..',
-  },
-  {
-    icon: Skill9,
-    title: 'Family and Estate Law',
-    description:
-      'Counsel on family matters, estate planning, trusts, and probate proceedings.',
-  },
-];
-
-// team data
-export const team = [
-  {
-    image: TeamMember3,
-    name: 'Balram Choubey',
-    position: 'Advocate on record Patna High Court',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
-  },
-  {
-    image: TeamMember2,
-    name: 'Balram Choubey',
-    position: 'Advocate on record Patna High Court',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
-  },
-  {
-    image: TeamMember1,
-    name: 'Balram Choubey',
-    position: 'Advocate on record Patna High Court',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
-  },
-];
-
 // social data
 export const social = [
   {
@@ -232,9 +99,3 @@ export const social = [
   },
 ];
 
-export const partner = [
-  {  img: axis },
-  { img:  hdfc },
-  { img: icic },
-  { img: kotak},
-];
